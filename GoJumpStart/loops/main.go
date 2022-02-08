8587d0681e2f01c24262b64a7869881523b2a747
mainuldip <mainuldip@gmail.com>
2022-02-07 23:16:36 -0500

    maps updating

--- GoJumpStart/loops/main.go
+++ GoJumpStart/loops/main.go
@@ -1,7 +1,37 @@
 package main
 
+import (
+	"fmt"
+	"strconv"
+)
+
 func main() {
 	// Long method
-
+	i := 1
+	for i <= 10 {
+		fmt.Println(i)
+		fmt.Println("Value of i is => " + strconv.Itoa(i))
+		fmt.Println(fmt.Sprint("Hello ", i))
+		// https://go.dev/doc/effective_go#printing
+		i++
+	}
 	// Short Method
+	for x := 0; x <= 10; x++ {
+		fmt.Println("Counter x is => " + strconv.Itoa(x))
+		fmt.Println("Counter x is => ", x)
+	}
+
+	// FixxBuzz
+
+	for y := 0; y <= 100; y++ {
+		if y%15 == 0 {
+			fmt.Println("FizzBuzz")
+		} else if y%3 == 0 {
+			fmt.Println("Fizz")
+		} else if y%5 == 0 {
+			fmt.Println("Buzz")
+		} else {
+			fmt.Println(y)
+		}
+	}
 }

393eac12dc2ac144738e638035dda971b35bdd05
mainuldip <mainuldip@gmail.com>
2022-02-08 22:33:04 -0500

    find some more interface sruct implementation

--- GoJumpStart/web/main.go
+++ GoJumpStart/web/main.go
@@ -1,5 +1,21 @@
 package main
 
+import (
+	"fmt"
+	"net/http"
+)
+
+func index(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "<h1>http from golang programming</h2>")
+}
+
+func about(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "<h1>Golang About Page Cool</h1>")
+}
+
 func main() {
-	println("Good Web")
+	http.HandleFunc("/", index)
+	http.HandleFunc("/about", about)
+	println("Server Starting Port 3000")
+	http.ListenAndServe(":3000", nil)
 }
